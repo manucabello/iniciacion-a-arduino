1cccc57cbf5764923ddbab3c4aaf7371a86b6a93
Manu Cabello <manu@Mac-mini-de-Manu.local>
2018-01-30 21:30:37 +0100

    Summary: Primera subida del código del Tutorial 5/9 de Iniciación a Arduino
    Details: Se ha subido el código relativo al circuito 2 y 3 del tutorial

---
+++ Tutorial5-EntradasDigitales/2Buttons/2Buttons.ino
@@ -0,0 +1,39 @@
+/*
+ * 
+ * https://www.manusoft.es
+ * TUTORIALES Y SOLUCIONES INFORMÁTICAS
+ * 
+ * Iniciación a Arduino [5/9]: Salidas Digitales - Botones.
+ * 
+ */
+
+// constants won't change. They're used here to set pin numbers:
+const int buttonAPin = 2;     // the number of the pushbutton A pin
+const int buttonBPin = 4;     // the number of the pushbutton B pin
+const int ledPin =  8;      // the number of the LED pin
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton A pin as an input:
+  pinMode(buttonAPin, INPUT);
+  // initialize the pushbutton B pin as an input:
+  pinMode(buttonBPin, INPUT);
+}
+
+void loop() {
+  // check if the pushAbutton is pressed.
+  // if it is, the LED is HIGH:
+  if (digitalRead(buttonAPin) == LOW) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+  }
+  // check if the pushBbutton is pressed.
+  // if it is, the LED is LOW:
+  if (digitalRead(buttonBPin) == LOW) {
+    digitalWrite(ledPin, LOW);
+  }
+}
